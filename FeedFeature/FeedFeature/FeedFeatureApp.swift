2612cbd4ee214c087156897cf6fc0eb29d4b9dfd
Galih Samodra Wicaksono <>
2023-05-03 14:00:44 +0700

    wip initial push

---
+++ FeedFeature/FeedFeature/FeedFeatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedFeatureApp.swift
+//  FeedFeature
+//
+//  Created by Galih Samodra Wicaksono on 03/05/23.
+//
+
+import SwiftUI
+
+@main
+struct FeedFeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
