70df1bc25e762f2494083589d16fcf0f6a1e1d87
Galih Samodra Wicaksono <samudragalih94@gmail.com>
2023-05-03 14:35:13 +0700

    Create the FeedItem Model

---
+++ FeedFeature/FeedFeature/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  FeedFeature
+//
+//  Created by Galih Samodra Wicaksono on 03/05/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
