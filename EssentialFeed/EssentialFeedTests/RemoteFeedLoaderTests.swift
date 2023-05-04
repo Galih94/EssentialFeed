bc9b16f643dbd662b58ee167c8e7db5be3a7e3cc
Galih Samodra Wicaksono <samudragalih94@gmail.com>
2023-05-04 10:20:00 +0700

    feat(tests): create test test_load_requestDataFromURL to validate requestedURL when load RemoteFeedLoader

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,7 +8,9 @@
 import XCTest
 
 class RemoteFeedLoader {
-     
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+    }
 }
 
 class HTTPClient {
@@ -25,6 +27,15 @@ final class RemoteFeedLoaderTests: XCTestCase {
         _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 
 }
