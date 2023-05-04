c93dd8d1b64203b507097bbccfc87dd1c423e917
Galih Samodra Wicaksono <samudragalih94@gmail.com>
2023-05-04 10:57:00 +0700

    feat(tests): create makeSUT to reuse compose RemoteFeedLoader

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -35,8 +35,7 @@ class HTTPClientSpy: HTTPClient {
 final class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClientSpy()
-        let url = URL(string: "https://a-url.com")!
-        _ = RemoteFeedLoader(url: url, client: client)
+        _ = makeSUT(client: client)
         
         XCTAssertNil(client.requestedURL)
     }
@@ -44,11 +43,15 @@ final class RemoteFeedLoaderTests: XCTestCase {
     func test_load_requestDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
         let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url,client: client)
+        let sut = makeSUT(url: url,client: client)
         
         sut.load()
         
         XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!, client: HTTPClient) -> RemoteFeedLoader {
+        return RemoteFeedLoader(url: url , client: client)
     }
 
 }
