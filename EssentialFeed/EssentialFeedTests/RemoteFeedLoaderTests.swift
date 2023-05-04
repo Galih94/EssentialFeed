69d465d32678c29aafb0ae77361d56ee9ef836ca
Galih Samodra Wicaksono <samudragalih94@gmail.com>
2023-05-04 10:11:50 +0700

    rename(tests): rename test_init to test_init_doesNotRequestDataFromURL

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -16,9 +16,9 @@ class HTTPClient {
 }
 
 final class RemoteFeedLoaderTests: XCTestCase {
-    func test_init() {
+    func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClient()
-        let sut = RemoteFeedLoader()
+        _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestedURL)
     }
