1d3b8d16b06bc6679f1e78b0a02472735b0d2d5c
Galih Samodra Wicaksono <samudragalih94@gmail.com>
2023-05-04 13:53:21 +0700

    feat(tests): remove stubbing error on client and change to saved completion on client

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -36,11 +36,13 @@ final class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
-        client.error = NSError(domain: "Test", code: 0)
+        
         var capturedErrors = [RemoteFeedLoader.Error]()
         sut.load {
             capturedErrors.append($0)
         }
+        let clientsError = NSError(domain: "Test", code: 0)
+        client.completions[0](clientsError)
         
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
@@ -56,12 +58,10 @@ final class RemoteFeedLoaderTests: XCTestCase {
     private class HTTPClientSpy: HTTPClient {
         
         var requestedURLs = [URL]()
-        var error: Error?
+        var completions = [(Error) -> Void]()
         
         func get(from url: URL, completion: @escaping (Error) -> Void) {
-            if let error = error {
-                completion(error)
-            }
+            completions.append(completion)
             requestedURLs.append(url)
         }
     }
