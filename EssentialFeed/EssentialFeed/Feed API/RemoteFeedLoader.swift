4af3e4cd766ecfb28b3b4738d65f5973b421ae2c
Galih Samodra Wicaksono <samudragalih94@gmail.com>
2023-05-04 14:32:28 +0700

    feat(HTTPClient): change get result from Error to nilable Error and HTTPURLResponse

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (Error) -> Void)
+    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
 }
 
 public final class RemoteFeedLoader {
@@ -25,8 +25,12 @@ public final class RemoteFeedLoader {
         self.url = url
     }
     public func load(completion: @escaping (Error) -> Void ) {
-        client.get(from: url) { error in
-            completion(.connectivity)
+        client.get(from: url) { (error, response) in
+            if response != nil {
+                completion(.invalidData)
+            } else {
+                completion(.connectivity)
+            }
         }
     }
 }
