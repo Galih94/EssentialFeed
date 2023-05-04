8509e98caf57a318501c03b1091d048b3167f936
Galih Samodra Wicaksono <samudragalih94@gmail.com>
2023-05-04 11:01:32 +0700

    feat(tests): change makeSUT return couple RemoteFeedLoader and HTTPClientSpy

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -34,24 +34,24 @@ class HTTPClientSpy: HTTPClient {
 
 final class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClientSpy()
-        _ = makeSUT(client: client)
+        let (_, client) = makeSUT()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
-        let client = HTTPClientSpy()
-        let sut = makeSUT(url: url,client: client)
+        let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
         XCTAssertEqual(client.requestedURL, url)
     }
     
-    private func makeSUT(url: URL = URL(string: "https://a-url.com")!, client: HTTPClient) -> RemoteFeedLoader {
-        return RemoteFeedLoader(url: url , client: client)
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (RemoteFeedLoader, HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url , client: client)
+        return (sut, client)
     }
 
 }
