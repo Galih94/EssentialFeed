1c8f282202b1e3888c522d494196a2a29dca6833
Galih Samodra Wicaksono <samudragalih94@gmail.com>
2023-05-08 15:47:34 +0700

    feat(RemoteFeedLoader): update using LoadFeedResult in load RemoteFeedLoader, make Result as type alias of LoadFeedResult

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -16,23 +16,21 @@ public final class RemoteFeedLoader {
         case invalidData
     }
     
-    public enum Result: Equatable {
-        case success([FeedItem])
-        case failure(Error)
-    }
+    public typealias Result = LoadFeedResult
     
     public init (url: URL, client: HTTPClient) {
         self.client = client
         self.url = url
     }
-    public func load(completion: @escaping (Result) -> Void ) {
+    
+    public func load(completion: @escaping (LoadFeedResult) -> Void ) {
         client.get(from: url) { [weak self] (result) in
             guard self != nil else { return }
             switch result {
             case let .success(data, response):
                 completion(FeedItemsMapper.map(data, from: response))
             case .failure( _):
-                completion(.failure(.connectivity))
+                completion(.failure(Error.connectivity))
             }
         }
     }
