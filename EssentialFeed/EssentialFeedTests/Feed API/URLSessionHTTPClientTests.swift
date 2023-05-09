be611599bed99882eb498ce108a0eb116c259d67
Galih Samodra Wicaksono <samudragalih94@gmail.com>
2023-05-09 14:44:04 +0700

    feat(tests): add missing super.setUp() and super.tearDown()

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -30,10 +30,12 @@ final class URLSessionHTTPClient {
 final class URLSessionHTTPClientTests: XCTestCase {
     
     override func setUp() {
+        super.setUp()
         URLProtocolStub.startInterceptingRequests()
     }
     
     override func tearDown() {
+        super.tearDown()
         URLProtocolStub.stopInterceptingRequests()
     }
     
