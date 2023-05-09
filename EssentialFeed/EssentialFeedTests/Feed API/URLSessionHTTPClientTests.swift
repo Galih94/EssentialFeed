d49289b730ea6b595661387c8e8d6aaffaae9b3a
Galih Samodra Wicaksono <samudragalih94@gmail.com>
2023-05-09 11:12:25 +0700

    feat(tests): create test_getFromURL_resumeDataTaskWithURL to test resume on URLSessionHTTPClient by spying URLSessionDataTaskSpy that is stubbed it into URLSessionSpy

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -18,7 +18,7 @@ final class URLSessionHTTPClient {
     func get(from url: URL) {
         session.dataTask(with: url) {_, _,_ in
             
-        }
+        }.resume()
     }
 }
 
@@ -36,22 +36,48 @@ final class URLSessionHTTPClientTests: XCTestCase {
         XCTAssertEqual(session.receivedURL, [url])
     }
     
+    func test_getFromURL_resumeDataTaskWithURL() {
+        let url = URL(string: "http://any-url.com")!
+        let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        session.stub(with: url, for: task)
+        let sut = URLSessionHTTPClient(session: session)
+        sut.get(from: url)
+        
+        XCTAssertEqual(task.resumeCallCount, 1)
+    }
+    
     //MARK: - Helper Tests code
     
     private final class URLSessionSpy: URLSession {
         var receivedURL = [URL]()
-        
+        private var stubs = [URL: URLSessionDataTask]()
         override init(){}
+        
+        func stub(with url: URL, for task: URLSessionDataTask) {
+            stubs[url] = task
+        }
         
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
             receivedURL.append(url)
             let dataTask = FakeURLSessionDataTask()
-            return dataTask
+            return stubs[url] ?? dataTask
         }
     }
     
     private final class FakeURLSessionDataTask: URLSessionDataTask {
         override init(){}
+        
+        override func resume() {}
+    }
+    
+    private final class URLSessionDataTaskSpy: URLSessionDataTask {
+        override init(){}
+        
+        var resumeCallCount = 0
+        override func resume() {
+            resumeCallCount += 1
+        }
     }
 
 }
