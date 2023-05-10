f110268e4db665c806586af8154c69176e8d6911
Galih Samodra Wicaksono <samudragalih94@gmail.com>
2023-05-10 10:25:43 +0700

    feat(EssentialFeedAPIEndToEndTests): create target test EssentialFeedAPIEndToEndTests.xcscheme to separate end to end tests with the rest of the unit tests because it's depends on many factor such us internet connection to get it success

---
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialFeedAPIEndToEndTests.swift
+//  EssentialFeedAPIEndToEndTests
+//
+//  Created by Galih Samodra Wicaksono on 10/05/23.
+//
+
+import XCTest
+import EssentialFeed
+
+final class EssentialFeedAPIEndToEndTests: XCTestCase {
+
+}
