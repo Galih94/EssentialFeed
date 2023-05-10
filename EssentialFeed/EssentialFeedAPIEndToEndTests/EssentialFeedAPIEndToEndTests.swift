2d08a6f4feb63e31e381da7b1482b7dbceec144e
Galih Samodra Wicaksono <samudragalih94@gmail.com>
2023-05-10 10:29:29 +0700

    feat(EssentialFeedAPIEndToEndTests): create test_endToEndTestServerGETFeedResult_matchedFixedTestAccountData on separate tests EssentialFeedAPIEndToEndTests with call server url and time out 20 second

--- EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -9,5 +9,27 @@ import XCTest
 import EssentialFeed
 
 final class EssentialFeedAPIEndToEndTests: XCTestCase {
+    
+    func test_endToEndTestServerGETFeedResult_matchedFixedTestAccountData() {
+        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let client = URLSessionHTTPClient()
+        let sut = RemoteFeedLoader(url: testServerURL, client: client)
+        
+        var receivedResult: LoadFeedResult?
+        let exp = expectation(description: "Waiting for load")
+        sut.load { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 20.0)
+        switch receivedResult {
+        case let .success(items):
+            XCTAssertEqual(items.count, 8)
+        case let .failure(error):
+            XCTFail("Expected succcess got \(error) instead")
+        default:
+            XCTFail("Expected succcess got nil instead")
+        }
+    }
 
 }
