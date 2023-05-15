88b6daf35aa27fd9417fbae3e3d97b842c255511
galih <samudragalih94@gmail.com>
2023-05-15 13:09:35 +0700

    feat(tests): create LoadFeedFromCacheUseCaseTests

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -0,0 +1,12 @@
+//
+//  LoadFeedFromCacheUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Galih Samudra on 15/05/23.
+//
+
+import XCTest
+
+final class LoadFeedFromCacheUseCaseTests: XCTestCase {
+
+}
