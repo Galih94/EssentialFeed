f70658b2c21c5f181b9a0c56a58d908509c572b8
galih <samudragalih94@gmail.com>
2023-05-15 10:45:32 +0700

    refactor(RemoteFeedItem): refactor FeedItemsMapper change internal representation Item into RemoteFeedItem to decouple the Items mapper from data models

--- EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -7,34 +7,28 @@
 
 import Foundation
 
+
+
+internal struct RemoteFeedItem: Decodable { // make internal representation from FeedItem so it can map into FeedItem without changing FeedItem Implementation
+    internal let id: UUID
+    internal let description: String?
+    internal let location: String?
+    internal let image: URL
+}
+
 internal final class FeedItemsMapper {
     
     private static var OK_200: Int { return 200 }
     private struct Root: Decodable {
-        let items: [Item]
-        
-        var feed: [FeedItem] {
-            return items.map({ $0.item })
-        }
-    }
-
-    private struct Item: Decodable { // make internal representation from FeedItem so it can map into FeedItem without changing FeedItem Implementation
-        let id: UUID
-        let description: String?
-        let location: String?
-        let image: URL
-        
-        var item: FeedItem {
-            return FeedItem(id: id, description: description, location: location, imageURL: image)
-        }
+        let items: [RemoteFeedItem]
     }
     
-    internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+    internal static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
         guard let root = try? JSONDecoder().decode(Root.self, from: data),
                 response.statusCode == OK_200 else {
-            return .failure(RemoteFeedLoader.Error.invalidData)
+            throw RemoteFeedLoader.Error.invalidData
         }
         
-        return .success(root.feed)
+        return root.items
     }
 }
