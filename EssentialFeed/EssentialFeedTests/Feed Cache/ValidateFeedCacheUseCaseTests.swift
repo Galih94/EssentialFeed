8d73e075406ea99980248031bf5ea30141b76406
galih <samudragalih94@gmail.com>
2023-05-16 11:17:47 +0700

    feat(ValidateFeedCacheUseCaseTests): create ValidateFeedCacheUseCaseTests to test use case validate feed

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -0,0 +1,11 @@
+//
+//  ValidateFeedCacheUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Galih Samudra on 16/05/23.
+//
+
+import XCTest
+import EssentialFeed
+
+final class ValidateFeedCacheUseCaseTests: XCTestCase {}
