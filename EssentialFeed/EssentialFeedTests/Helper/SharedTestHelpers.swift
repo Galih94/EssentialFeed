d5283dcd87c566541960a88fdda5981c8f531710
galih <samudragalih94@gmail.com>
2023-05-16 11:57:47 +0700

    feat(Helpers) create SharedTestHelpers and FeedCacheTestHelpers to extract shared duplicate helpers into shared scope

---
+++ EssentialFeed/EssentialFeedTests/Helper/SharedTestHelpers.swift
@@ -0,0 +1,16 @@
+//
+//  SharedTestHelpers.swift
+//  EssentialFeedTests
+//
+//  Created by Galih Samudra on 16/05/23.
+//
+
+import Foundation
+
+func anyNSError() -> NSError {
+    return NSError(domain: "any error domain", code: 1)
+}
+
+func anyURL() -> URL {
+    return URL(string: "http://any-url.com")!
+}
