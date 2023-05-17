2513d0f4b1e828723264eccc8ba7322ebc752ac8
galih <samudragalih94@gmail.com>
2023-05-17 11:29:57 +0700

    feat(tests): create test_retrieve_deliversEmptyOnEmptyCache to test retrieve empty cache from empty store

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -6,7 +6,32 @@
 //
 
 import XCTest
+import EssentialFeed
+
+class CodableFeedStore {
+    func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
+        completion(.empty)
+    }
+    
+}
 
 final class CodableFeedStoreTests: XCTestCase {
+    
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+         let sut = CodableFeedStore()
+        
+        let exp = expectation(description: "Waiting for retrieval")
+        sut.retrieve { result in
+            switch result {
+            case .empty:
+                break
+            default:
+                XCTFail("Expected empty got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
 
 }
