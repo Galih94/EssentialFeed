22740df41b2b9cf49984d1d2f741bf6d7d9c1f2d
galih <samudragalih94@gmail.com>
2023-05-17 11:28:49 +0700

    feat(CodableFeedStoreTests): create CodableFeedStoreTests

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -0,0 +1,12 @@
+//
+//  CodableFeedStoreTests.swift
+//  EssentialFeedTests
+//
+//  Created by Galih Samudra on 17/05/23.
+//
+
+import XCTest
+
+final class CodableFeedStoreTests: XCTestCase {
+
+}
