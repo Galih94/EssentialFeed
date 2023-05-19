8112732c0bf6f8930e8dd8aba82f542ce565f3ec
galih <samudragalih94@gmail.com>
2023-05-19 11:11:35 +0700

    feat(interface): add new method to check side-effects on separate tests

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -14,8 +14,12 @@ protocol FeedStoreSpecs {
     func test_retrieve_deliversFoundValuesOnNonEmptyCache()
     func test_retrieve_hasNoSdeEffectsOnNonEmptyCache()
     
+    func test_insert_deliversNoErrorOnEmptyCache()
+    func test_insert_deliversNoErrorOnNonEmptyCache()
     func test_insert_overridePreviouslyInsertedCacheValues()
     
+    func test_delete_deliversNoErrorOnEmptyCache()
+    func test_delete_deliversNoErrorOnNonEmptyCache()
     func test_delete_hasNoSideEffectsOnEmptyCache()
     func test_delete_emptiesPreviouslyInsertedCache()
     
@@ -29,10 +33,12 @@ protocol FailableRetrieveFeedStoreSpecs {
 
 protocol FailableInsertFeedStoreSpecs {
     func test_insert_deliversErrorOnInsertionError()
+    func test_insert_hasNoSideEffectOnInsertionError()
 }
 
 protocol FailableDeleteFeedStoreSpecs {
     func test_delete_deliversErrorOnDeletionError()
+    func test_delete_hasNoSideEffectOnDeletionError()
 }
 
 
