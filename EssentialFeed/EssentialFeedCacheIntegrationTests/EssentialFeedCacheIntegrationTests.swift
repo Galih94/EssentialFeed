5481c33779d96efd5fa0cf7d1c1703f7b275b332
galih <samudragalih94@gmail.com>
2023-05-22 09:59:09 +0700

    feat(EssentialFeedCacheIntegrationTests): create `EssentialFeedCacheIntegrationTests`  to separate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Galih Samudra on 22/05/23.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+}
+ 
