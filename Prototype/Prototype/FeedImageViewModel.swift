2fbacc2243c91105c3814ffc1ebb13a6dbb0a1d0
galih <samudragalih94@gmail.com>
2023-05-24 11:46:53 +0700

    feat(FeedImageViewModel): create `FeedImageViewModel` and add it's prototype data

---
+++ Prototype/Prototype/FeedImageViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedImageViewModel.swift
+//  Prototype
+//
+//  Created by Galih Samudra on 24/05/23.
+//
+
+struct FeedImageViewModel {
+    let description: String?
+    let location: String?
+    let imageName: String
+}
