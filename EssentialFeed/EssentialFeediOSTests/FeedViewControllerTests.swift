c3fb1f2419e1d7b786e9343d7f407f1228c13b40
galih <samudragalih94@gmail.com>
2023-05-29 13:20:35 +0700

    feat(tests): create tests test_init_doesNotLoadFeed make sure not load on creation FeedViewController

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -7,5 +7,25 @@
 
 import XCTest
 
+final class FeedViewController {
+    let loader: FeedViewControllerTests.LoaderSpy
+    
+    init(loader: FeedViewControllerTests.LoaderSpy) {
+        self.loader = loader
+    }
+}
+
 final class FeedViewControllerTests: XCTestCase {
+    func test_init_doesNotLoadFeed() {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        
+        XCTAssertEqual(loader.loadCallCount, 0)
+    }
+    
+    // MARK: Helpers
+    
+    class LoaderSpy {
+        private(set) var loadCallCount: Int = 0
+    }
 }
