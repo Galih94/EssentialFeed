0aa0e4d3106275d8e3cb28cbcf26508b0e6150ae
galih <samudragalih94@gmail.com>
2023-05-29 13:16:46 +0700

    feat(FeedViewControllerTests): create `FeedViewControllerTests`

---
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -0,0 +1,11 @@
+//
+//  FeedViewControllerTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Galih Samudra on 29/05/23.
+//
+
+import XCTest
+
+final class FeedViewControllerTests: XCTestCase {
+}
