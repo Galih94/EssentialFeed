8e11f1a31330e192f615f62f0617c40c831510fa
galih <samudragalih94@gmail.com>
2023-06-05 10:56:50 +0700

    feat(FeedRefreshViewController): remove callback `onRefresh` to `onFeedLoad` inside `FeedViewModel` to remove `EssentialFeed` dependency on `FeedRefreshViewController` and change `State` enum to `isLoading` boolean

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -6,11 +6,10 @@
 //
 
 import UIKit
-import EssentialFeed
 
 final class FeedRefreshViewController: NSObject {
     private let viewModel: FeedViewModel
-    var onRefresh: (([FeedImage]) -> Void)?
+
     private(set) lazy var view: UIRefreshControl = binded(UIRefreshControl())
     
     init(viewModel: FeedViewModel) {
@@ -27,9 +26,6 @@ final class FeedRefreshViewController: NSObject {
                 self?.view.beginRefreshing()
             } else {
                 self?.view.endRefreshing()
-            }
-            if let feed = viewModel.feed {
-                self?.onRefresh?(feed)
             }
         }
         view.addTarget(self, action: #selector(refresh), for: .valueChanged)
--- EssentialFeed/EssentialFeediOS/Feed UI/Models/FeedViewModel.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Models/FeedViewModel.swift
@@ -7,47 +7,26 @@
 import EssentialFeed
 
 final class FeedViewModel {
-    private enum State {
-        case pending
-        case loading
-        case loaded([FeedImage])
-        case failed
-    }
     private let feedLoader: FeedLoader?
     var onChange: ( (FeedViewModel) -> Void )?
-    private var state = State.pending {
-        didSet { onChange?(self) }
-    }
-    var isLoading: Bool {
-        switch state {
-        case .loading:
-            return true
-        case .loaded, .pending, .failed:
-            return false
+    var onFeedLoad: (([FeedImage]) -> Void)?
+    var isLoading: Bool = false {
+        didSet {
+            onChange?(self)
         }
     }
-    var feed: [FeedImage]? {
-        switch state {
-        case .loading, .pending, .failed:
-            return nil
-        case let .loaded(feed):
-            return feed
-        }
-    }
-    
     
     init(feedLoader: FeedLoader? = nil) {
         self.feedLoader = feedLoader
     }
     
     func loadFeed() {
-        state = .loading
+        isLoading = true
         feedLoader?.load(completion: { [weak self] result in
             if let feed = try? result.get() {
-                self?.state = .loaded(feed)
-            } else {
-                self?.state = .failed
+                self?.onFeedLoad?(feed)
             }
+            self?.isLoading = false
         })
     }
 }
