02a9216254c7f5e73d14258f12fcd10c25801e1a
galih <samudragalih94@gmail.com>
2023-06-05 12:39:32 +0700

    feat(FeedImageViewModel): move `FeedImageViewModel` to it's own file

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -6,54 +6,6 @@
 //
 
 import UIKit
-import EssentialFeed
-
-final class FeedImageViewModel {
-    private var task: FeedImageLoaderTask?
-    private let model: FeedImage
-    private let imageLoader: FeedImageLoader
-    var onImageLoad: ((UIImage?) -> Void)?
-    var onImageLoadingStateChange: ((Bool) -> Void)?
-    var onShouldRetryImageLoadStateChange: ((Bool) -> Void)?
-    
-    var hasLocation: Bool {
-        return location != nil
-    }
-    
-    var location: String? {
-        return model.location
-    }
-    
-    var description: String? {
-        return model.description
-    }
-    
-    
-    init(model: FeedImage, imageLoader: FeedImageLoader) {
-        self.model = model
-        self.imageLoader = imageLoader
-    }
-    func loadImageData() {
-        onImageLoadingStateChange?(true)
-        onShouldRetryImageLoadStateChange?(false)
-        task = self.imageLoader.loadImageData(from: self.model.url, completion: { [weak self] result in
-            let data = try? result.get()
-            let dataImage = data.flatMap(UIImage.init) ?? nil
-            if let image = dataImage {
-                self?.onImageLoad?(image)
-            } else {
-                self?.onShouldRetryImageLoadStateChange?(true)
-            }
-            self?.onImageLoadingStateChange?(false)
-        })
-    }
-    
-    func cancelImageDataLoad() {
-        task?.cancel()
-        task = nil
-    }
-    
-}
 
 final class FeedImageCellController {
     private let viewModel: FeedImageViewModel
