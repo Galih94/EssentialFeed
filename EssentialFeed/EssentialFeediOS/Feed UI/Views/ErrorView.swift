e5049d74eef40ceccce3ef43947210963b0afbd4
galih <samudragalih94@gmail.com>
2023-06-12 20:28:50 +0700

    feat(ErrorView): move `ErrorView` to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Galih Samudra on 12/06/23.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
