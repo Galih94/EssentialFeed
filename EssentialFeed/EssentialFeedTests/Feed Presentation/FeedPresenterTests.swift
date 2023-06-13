38b38635751a63ec976bc5e95d15f0036b3ca164
galih <samudragalih94@gmail.com>
2023-06-13 11:00:13 +0700

    feat(tests): create helper makeSUT

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -17,12 +17,20 @@ final class FeedPresenter {
 final class FeedPresenterTests: XCTestCase {
     
     func test_init_doesNotSendMessagesToView() {
-        let view = ViewSpy()
+        let (_, view) = makeSUT()
         _ = FeedPresenter(view: view)
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messsages")
     }
     
     // MARK: -- Helpers
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (FeedPresenter, ViewSpy) {
+        let view = ViewSpy()
+        let sut = FeedPresenter(view: view)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(view, file: file, line: line)
+        return (sut, view)
+    }
+    
     private class ViewSpy {
         let messages = [Any]()
     }
