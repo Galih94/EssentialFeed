86c9d8359eb38578aeb8511bbf1d65e229cac83a
galih <samudragalih94@gmail.com>
2023-06-13 14:10:01 +0700

    feat(ViewModel): move view model to each own file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Galih Samudra on 13/06/23.
+//
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Galih Samudra on 13/06/23.
+//
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
