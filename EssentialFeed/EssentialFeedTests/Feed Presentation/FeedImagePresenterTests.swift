00de4206527fdedbc3800b717fe1a1dac889e83c
galih <samudragalih94@gmail.com>
2023-06-13 16:54:44 +0700

    feat(tests): Inline Data creation

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -88,9 +88,8 @@ final class FeedImagePresenterTests: XCTestCase {
     func test_didFinishLoadingImageData_displayRetryOnFailedImageTransformation() {
         let (sut, view) = makeSUT(imageTransformer: fail)
         let image = uniqueImage()
-        let data = Data()
 
-        sut.didFinishLoadingImageData(with: data, for: image)
+        sut.didFinishLoadingImageData(with: Data(), for: image)
         
         guard let message = view.messages.first else {
             return XCTFail("Expected found messages")
@@ -105,11 +104,10 @@ final class FeedImagePresenterTests: XCTestCase {
     
     func test_didFinishLoadingImageData_displayImageOnSuccessfulImageTransformation() {
         let image = uniqueImage()
-        let data = Data()
         let transformedData = AnyImage()
         let (sut, view) = makeSUT(imageTransformer: { _ in transformedData })
 
-        sut.didFinishLoadingImageData(with: data, for: image)
+        sut.didFinishLoadingImageData(with: Data(), for: image)
         
         guard let message = view.messages.first else {
             return XCTFail("Expected found messages")
