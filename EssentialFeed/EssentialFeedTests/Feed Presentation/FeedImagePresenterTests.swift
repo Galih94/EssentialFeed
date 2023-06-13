39ac3c94eef7185ed42a7d5fae754fa8b34baea6
galih <samudragalih94@gmail.com>
2023-06-13 15:33:39 +0700

    feat(FeedImagePresenter): `FeedImagePresenter` displays loading image on `didStartLoadingImageData`

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -6,15 +6,61 @@
 //
 
 import XCTest
+import EssentialFeed
+
+protocol FeedImageView {
+    func display(_ model: FeedImageViewModel)
+}
+
+struct FeedImageViewModel {
+    let description: String?
+    let location: String?
+    let image: Any?
+    let isLoading: Bool
+    let shouldRetry: Bool
+    
+    var hasLocation: Bool {
+        return location != nil
+    }
+}
 
 final class FeedImagePresenter {
-    init(view: Any) {}
+    private let view: FeedImageView
+    init(view: FeedImageView) {
+        self.view = view
+    }
+    
+    func didStartLoadingImageData(for model: FeedImage) {
+        view.display(
+            FeedImageViewModel(description: model.description,
+                               location: model.location,
+                               image: nil,
+                               isLoading: true,
+                               shouldRetry: false))
+    }
 }
 
 final class FeedImagePresenterTests: XCTestCase {
     func test_init_doesNotSendMessagesToView() {
         let (_, view) = makeSUT()
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messaged")
+    }
+    
+    func test_didStartLoadingImageData_displayLoadingImage() {
+        let (sut, view) = makeSUT()
+        let image = uniqueImage()
+        
+        sut.didStartLoadingImageData(for: image)
+        
+        guard let message = view.messages.first else {
+            return XCTFail("Expected found messages")
+        }
+        XCTAssertEqual(view.messages.count, 1)
+        XCTAssertEqual(message.description, image.description)
+        XCTAssertEqual(message.location, image.location)
+        XCTAssertNil(message.image)
+        XCTAssertEqual(message.isLoading, true)
+        XCTAssertEqual(message.shouldRetry, false)
     }
     
     // MARK: -- Helpers
@@ -27,7 +73,11 @@ final class FeedImagePresenterTests: XCTestCase {
         return (sut, view)
     }
     
-    private final class ViewSpy {
-        let messages = [Any]()
+    private final class ViewSpy: FeedImageView {
+        var messages = [FeedImageViewModel]()
+        
+        func display(_ model: FeedImageViewModel) {
+            messages.append(model)
+        }
     }
 }
