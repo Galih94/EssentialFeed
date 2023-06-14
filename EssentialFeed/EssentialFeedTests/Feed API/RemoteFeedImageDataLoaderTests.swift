1372eac41e4fd1880a4dd361302e4c3abc9b6e15
galih <samudragalih94@gmail.com>
2023-06-14 17:10:03 +0700

    feat(RemoteFeedImageDataLoaderTests): create `RemoteFeedImageDataLoaderTests`

---
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -0,0 +1,10 @@
+//
+//  RemoteFeedImageDataLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Galih Samudra on 14/06/23.
+//
+
+import XCTest
+
+final class RemoteFeedImageDataLoaderTests: XCTestCase {}
