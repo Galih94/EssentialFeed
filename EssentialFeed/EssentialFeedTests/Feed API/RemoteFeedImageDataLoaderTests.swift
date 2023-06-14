e98a4d5342dc314cbd8cdf708321d5f93f5d42a4
galih <samudragalih94@gmail.com>
2023-06-14 17:24:31 +0700

    feat(RemoteFeedImageDataLoader): create `test_init_doesNotPerformAnyURLRequest` to make sure `RemoteFeedImageDataLoader` not perform any url request on initialization

--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -6,5 +6,24 @@
 //
 
 import XCTest
+import EssentialFeed
 
-final class RemoteFeedImageDataLoaderTests: XCTestCase {}
+public final class RemoteFeedImageDataLoader {
+    init(client: Any) {}
+    
+}
+
+final class RemoteFeedImageDataLoaderTests: XCTestCase {
+    
+    func test_init_doesNotPerformAnyURLRequest() {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedImageDataLoader(client: client)
+        
+        XCTAssertEqual(client.requestedURLs, [])
+    }
+    
+    // MARK: - Helpers
+    private class HTTPClientSpy {
+        var requestedURLs = [URL]()
+    }
+}
