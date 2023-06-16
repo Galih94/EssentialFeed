fe1e17fdcf28db6629dae6a4dfdd3113a635daff
galih <samudragalih94@gmail.com>
2023-06-16 11:34:57 +0700

    feat(LocalFeedImageDataLoader): create `test_init_doesOtMessageStoreUponCreation` to make sure `LocalFeedImageDataLoader` not send messages on creation

--- EssentialFeed/EssentialFeedTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
@@ -7,6 +7,21 @@
 
 import XCTest
 
+final class LocalFeedImageDataLoader {
+    init(store: Any) {}
+}
+
 final class LocalFeedImageDataLoaderTests: XCTestCase {
 
+    func test_init_doesOtMessageStoreUponCreation() {
+        let store = FeedStoreSpy()
+        _ = LocalFeedImageDataLoader(store: store)
+        
+        XCTAssertTrue(store.receivedMessages.isEmpty, "Expected no receivedMessages on initialization")
+    }
+    
+    // MARK: Helpers
+    private class FeedStoreSpy {
+        let receivedMessages = [Any]()
+    }
 }
