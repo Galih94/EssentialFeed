b9775e4e6cd92ea287dd9ad80f29da8236a64419
galih <samudragalih94@gmail.com>
2023-06-16 17:54:14 +0700

    feat(tests): LocalFeedImageDataLoader make sure not message upon creation

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedImageDataUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedImageDataUseCaseTests.swift
@@ -6,5 +6,23 @@
 //
 
 import XCTest
+import EssentialFeed
 
-final class CacheFeedImageDataUseCaseTests: XCTestCase {}
+final class CacheFeedImageDataUseCaseTests: XCTestCase {
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertTrue(store.receivedMessages.isEmpty)
+    }
+    
+    // MARK: -- Helpers
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (LocalFeedImageDataLoader, FeedImageDataStoreSpy) {
+        let store = FeedImageDataStoreSpy()
+        let sut = LocalFeedImageDataLoader(store: store)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(store, file: file, line: line)
+        
+        return (sut, store)
+    }
+    
+}
