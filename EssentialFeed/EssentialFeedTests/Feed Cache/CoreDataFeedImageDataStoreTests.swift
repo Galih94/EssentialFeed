7cf57fa7e7e1e16b8eda6b0265e8d40803f058d6
galih <samudragalih94@gmail.com>
2023-06-19 10:40:09 +0700

    feat(CoreDataFeedStore): make `CoreDataFeedStore` conform to `FeedImageDataStore` and test `retrieve` with `test_retrieveImageData_deliversNotFoundWhenEmpty`

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
@@ -6,7 +6,35 @@
 //
 
 import XCTest
+import EssentialFeed
+
+extension CoreDataFeedStore: FeedImageDataStore {
+    public func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void) {}
+    
+    public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+        completion(.success(.none))
+    }
+}
 
 final class CoreDataFeedImageDataStoreTests: XCTestCase {
+    
+    func test_retrieveImageData_deliversNotFoundWhenEmpty() {
+        let bundle = Bundle(for: CoreDataFeedStore.self)
+        let storeUrl = URL(filePath: "/dev/null")
+        let sut = try! CoreDataFeedStore(storeURL: storeUrl, bundle: bundle)
+        
+        
+        let url = anyURL()
+        let exp = expectation(description: "Wait for retrieval")
+        sut.retrieve(dataForURL: url) { result in
+            switch result {
+            case let .success(data):
+                XCTAssertEqual(data, .none)
+            default: XCTFail("Expected found empty data got \(result) isntead")
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
 
 }
