4ca01befea50d554a19e3b917313913f64450da0
galih <samudragalih94@gmail.com>
2023-06-19 10:07:52 +0700

    feat(CoreDataFeedImageDataStoreTests): create `CoreDataFeedImageDataStoreTests`

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
@@ -0,0 +1,12 @@
+//
+//  CoreDataFeedImageDataStoreTests.swift
+//  EssentialFeedTests
+//
+//  Created by Galih Samudra on 19/06/23.
+//
+
+import XCTest
+
+final class CoreDataFeedImageDataStoreTests: XCTestCase {
+
+}
