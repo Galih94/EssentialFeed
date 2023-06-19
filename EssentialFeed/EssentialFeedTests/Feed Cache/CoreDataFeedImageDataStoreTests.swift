d2d8d0743ae1fb205c6895455c99940d2852e2e6
galih <samudragalih94@gmail.com>
2023-06-19 12:05:15 +0700

    feat(CoreDataFeedStore): create `test_retrieveImageData_deliversNotFoundWhenStoredDataURLDoesNotMatch` test case get not found when url is mismatched

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
@@ -23,6 +23,31 @@ final class CoreDataFeedImageDataStoreTests: XCTestCase {
         
         expect(sut, toCompleteRetrieveWith: .success(.none), for: anyURL())
     }
+    
+    func test_retrieveImageData_deliversNotFoundWhenStoredDataURLDoesNotMatch() {
+        let sut = makeSUT()
+        let imageURL = URL(string: "http://image-url.com")!
+        let nonMatchingURL = URL(string: "http://another-url.com")!
+        
+        let data = anyData()
+        let exp = expectation(description: "Wait for insertion")
+        let image = LocalFeedImage(id: UUID(), description: "any description", location: "any location", url: imageURL)
+        sut.insert([image], timeStamp: Date()) { insertImageResult in
+            switch insertImageResult {
+            case let .failure(error): XCTFail("Expected success got \(error) intead")
+            case .success:
+                sut.insert(data, for: imageURL) { insertResult in
+                    if case let Result.failure(error) = insertResult {
+                        XCTFail("Failed insert data \(data) got \(error) intead")
+                    }
+                }
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+        
+        expect(sut, toCompleteRetrieveWith: .success(.none), for: nonMatchingURL)
+    }
 
     // MARK: Helpers
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CoreDataFeedStore {
