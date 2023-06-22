77dad39e5a33969ed28b41460c6eec654ca3c079
galih <samudragalih94@gmail.com>
2023-06-22 15:29:36 +0700

    feat(FeedImageDataLoaderCacheDecorator): create `FeedImageDataLoaderCacheDecorator` and test not send messages on init

---
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
@@ -0,0 +1,45 @@
+//
+//  FeedImageDataLoaderCacheDecoratorTests.swift
+//  EssentialAppTests
+//
+//  Created by Galih Samudra on 22/06/23.
+//
+
+import XCTest
+import EssentialFeed
+
+final class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
+    
+    private let decoratee: FeedImageDataLoader
+    
+    init(decoratee: FeedImageDataLoader) {
+        self.decoratee = decoratee
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> EssentialFeed.FeedImageDataLoaderTask {
+        return decoratee.loadImageData(from: url, completion: completion)
+    }
+    
+    
+}
+
+final class FeedImageDataLoaderCacheDecoratorTests: XCTestCase {
+    func test_init_doesNotLoadImageData() {
+        let loader = LoaderSpy()
+        let sut = FeedImageDataLoaderCacheDecorator(decoratee: loader)
+    }
+    
+    // MARK: Helper
+    private final class LoaderSpy: FeedImageDataLoader {
+        private struct Task: EssentialFeed.FeedImageDataLoaderTask {
+            func cancel() {}
+        }
+        private var messages = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
+        func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> EssentialFeed.FeedImageDataLoaderTask {
+            messages.append((url, completion))
+            return Task()
+        }
+        
+        
+    }
+}
