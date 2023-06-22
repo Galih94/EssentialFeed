fa87b2de9e72b7b5b0dcdf3aebb860c558187380
galih <samudragalih94@gmail.com>
2023-06-22 13:29:02 +0700

    feat(tests): extract create sut to factory method

--- EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -23,17 +23,24 @@ final class FeedLoaderCacheDecorator: FeedLoader {
 final class FeedLoaderCacheDecoratorTests: XCTestCase, FeedLoaderTestCase {
     func test_load_deliversFeedOnLoaderSuccess() {
         let feed = uniqueFeed()
-        let decoratee = FeedLoaderStub(result: .success(feed))
-        let sut = FeedLoaderCacheDecorator(decoratee: decoratee)
+        let sut = makeSUT(loaderResult: .success(feed))
 
         expect(sut, toCompleteWith: .success(feed))
     }
     
     func test_load_deliversFeedOnLoaderFailure() {
         let error = anyNSError()
-        let decoratee = FeedLoaderStub(result: .failure(error))
-        let sut = FeedLoaderCacheDecorator(decoratee: decoratee)
+        let sut = makeSUT(loaderResult: .failure(error))
 
         expect(sut, toCompleteWith: .failure(error))
+    }
+    
+    // MARK: Helpers
+    private func makeSUT(loaderResult: FeedLoader.Result, file: StaticString = #filePath, line: UInt = #line) -> FeedLoaderCacheDecorator {
+        let loader = FeedLoaderStub(result: loaderResult)
+        let sut = FeedLoaderCacheDecorator(decoratee: loader)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(loader, file: file, line: line)
+        return sut
     }
 }
