ffc0bd77dd1e15ba7ee95988d185bfcf22f4c718
galih <samudragalih94@gmail.com>
2023-06-28 11:14:19 +0700

    feat(Window): remove unnecessary Main storyboard - the initial set up happens in code

--- EssentialApp/EssentialAppTests/SceneDelegateTests.swift
+++ EssentialApp/EssentialAppTests/SceneDelegateTests.swift
@@ -10,6 +10,17 @@ import EssentialFeediOS
 @testable import EssentialApp
 
 final class SceneDelegateTests: XCTestCase {
+    func test_configureWindow_setsWindowAsKeyAndVisible() {
+        let window = UIWindowSpy()
+        let sut = SceneDelegate()
+        sut.window = window
+        
+        sut.configureWindow()
+        
+        XCTAssertEqual(window.makeKeyAndVisibleCallCount, 1, "Expected window to be the key window")
+        XCTAssertFalse(window.isHidden, "Expected window to be visible")
+    }
+    
     func test_sceneWillConnectToSession_configureRootsViewController() {
         let sut = SceneDelegate()
         sut.window = UIWindow()
@@ -24,4 +35,13 @@ final class SceneDelegateTests: XCTestCase {
         XCTAssertTrue(topController is FeedViewController, "Expected a feed view controller as top view controller, got \(String(describing: topController)) instead")
     }
 
+    // MARK: Helpers
+    private class UIWindowSpy: UIWindow {
+        var makeKeyAndVisibleCallCount = 0
+        override func makeKeyAndVisible() {
+            super.makeKeyAndVisible()
+            makeKeyAndVisibleCallCount = 1
+        }
+    }
+    
 }
