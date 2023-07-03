48931c6c57dad111533ec535fd942778eb3ebd0c
galih <samudragalih94@gmail.com>
2023-07-03 13:37:35 +0700

    feat(ImageComments): delivers proper results to 2xx response

--- EssentialFeed/EssentialFeed/Feed API/ImageCommentsMapper.swift
+++ EssentialFeed/EssentialFeed/Feed API/ImageCommentsMapper.swift
@@ -15,10 +15,14 @@ final class ImageCommentsMapper {
     
     static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
         guard let root = try? JSONDecoder().decode(Root.self, from: data),
-              response.isOK else {
+              isOK(response) else {
             throw RemoteImageCommentsLoader.Error.invalidData
         }
         
         return root.items
+    }
+    
+    private static func isOK(_ response: HTTPURLResponse) -> Bool {
+        return (200...299).contains(response.statusCode)
     }
 }
--- EssentialFeed/EssentialFeedTests/Feed API/LoadImageCommentFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/LoadImageCommentFromRemoteUseCaseTests.swift
@@ -42,9 +42,9 @@ final class LoadImageCommentFromRemoteUseCaseTests: XCTestCase {
         }
     }
     
-    func test_load_deliversErrorOnNon200HTTPResponse() {
+    func test_load_deliversErrorOnNon2xxHTTPResponse() {
         let (sut, client) = makeSUT()
-        let samples = [199, 201, 300, 400, 500]
+        let samples = [150, 199, 300, 400, 500]
         samples.enumerated().forEach { index, code in
             expect(sut, toCompleteWithResult: failure(.invalidData)) {
                 let json = makeItemsJSON(items: [])
@@ -53,23 +53,31 @@ final class LoadImageCommentFromRemoteUseCaseTests: XCTestCase {
         }
     }
     
-    func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
+    func test_load_deliversErrorOn2xxHTTPResponseWithInvalidJSON() {
         let (sut, client) = makeSUT()
-        expect(sut, toCompleteWithResult: failure(.invalidData)) {
-            let invalidJSON = Data("invalid json".utf8)
-            client.complete(withStatusCode: 200, data: invalidJSON) // need this to trigger error from client
+        let samples = [200, 201, 250, 280, 299]
+
+        samples.enumerated().forEach { index, code in
+            expect(sut, toCompleteWithResult: failure(.invalidData)) {
+                let invalidJSON = Data("invalid json".utf8)
+                client.complete(withStatusCode: code, data: invalidJSON, at: index) // need this to trigger error from client
+            }
         }
     }
     
-    func test_load_deliversErrorOn200HTTPResponseWithEmptyJSONList() {
+    func test_load_deliversErrorOn2xxHTTPResponseWithEmptyJSONList() {
         let (sut, client) = makeSUT()
-        expect(sut, toCompleteWithResult: .success([])) {
-            let emptyListJSON = makeItemsJSON(items: [])
-            client.complete(withStatusCode: 200, data: emptyListJSON) // need this to trigger error from client
+        let samples = [200, 201, 250, 280, 299]
+        
+        samples.enumerated().forEach { index, code in
+            expect(sut, toCompleteWithResult: .success([])) {
+                let emptyListJSON = makeItemsJSON(items: [])
+                client.complete(withStatusCode: code, data: emptyListJSON, at: index) // need this to trigger error from client
+            }
         }
     }
 
-    func test_load_deliversItemsOn200HTTPResponseWithJSONItems() {
+    func test_load_deliversItemsOn2xxHTTPResponseWithJSONItems() {
         let (sut, client) = makeSUT()
         let item1 = makeItem(
             id: UUID(),
@@ -85,9 +93,14 @@ final class LoadImageCommentFromRemoteUseCaseTests: XCTestCase {
         
         let items = [item1.model, item2.model]
         
-        expect(sut, toCompleteWithResult: .success(items)) {
-            let json = makeItemsJSON(items: [item1.json, item2.json])
-            client.complete(withStatusCode: 200, data: json) // need this to trigger error from client
+        
+        let samples = [200, 201, 250, 280, 299]
+        
+        samples.enumerated().forEach { index, code in
+            expect(sut, toCompleteWithResult: .success(items)) {
+                let json = makeItemsJSON(items: [item1.json, item2.json])
+                client.complete(withStatusCode: code, data: json, at: index) // need this to trigger error from client
+            }
         }
     }
     
