34dcf28025919255d6b976775fa58ffaf58e408d
galih <samudragalih94@gmail.com>
2023-07-03 16:47:43 +0700

    feat(RemoteFeedLoader): replace RemoteFeedLoader with generic RemoteLoader to remove duplication

--- EssentialFeed/EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
@@ -9,39 +9,6 @@ import XCTest
 import EssentialFeed
 
 final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
-    func test_init_doesNotRequestDataFromURL() {
-        let (_, client) = makeSUT()
-        
-        XCTAssertTrue(client.requestedURLs.isEmpty)
-    }
-    
-    func test_load_requestsDataFromURL() {
-        let url = URL(string: "https://a-given-url.com")!
-        let (sut, client) = makeSUT(url: url)
-        
-        sut.load{ _ in }
-        
-        XCTAssertEqual(client.requestedURLs, [url])
-    }
-    
-    func test_loadTwice_requestsDataFromURLTwice() {
-        let url = URL(string: "https://a-given-url.com")!
-        let (sut, client) = makeSUT(url: url)
-        
-        sut.load{ _ in }
-        sut.load{ _ in }
-        
-        XCTAssertEqual(client.requestedURLs, [url, url])
-    }
-    
-    func test_load_deliversErrorOnClientError() {
-        let (sut, client) = makeSUT()
-        expect(sut, toCompleteWithResult: failure(.connectivity)) {
-            let clientsError = NSError(domain: "Test", code: 0)
-            client.complete(with: clientsError) // need this to trigger error from client
-        }
-    }
-    
     func test_load_deliversErrorOnNon200HTTPResponse() {
         let (sut, client) = makeSUT()
         let samples = [199, 201, 300, 400, 500]
@@ -89,20 +56,6 @@ final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
             let json = makeItemsJSON(items: [item1.json, item2.json])
             client.complete(withStatusCode: 200, data: json) // need this to trigger error from client
         }
-    }
-    
-    func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
-        let url = URL(string: "http://any-url.com")!
-        let client = HTTPClientSpy()
-        var sut: RemoteFeedLoader? = RemoteFeedLoader(url: url, client: client)
-        var capturedResults = [RemoteFeedLoader.Result]()
-        sut?.load {
-            capturedResults.append($0)
-        }
-        sut = nil
-        let emptyListJSON = makeItemsJSON(items: [])
-        client.complete(withStatusCode: 200, data: emptyListJSON) // need this to trigger error from client
-        XCTAssertTrue(capturedResults.isEmpty)
     }
 
     
