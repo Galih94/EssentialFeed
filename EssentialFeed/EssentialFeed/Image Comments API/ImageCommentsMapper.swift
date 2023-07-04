a91fb0bef309994933b108e9b723a10044189846
galih <samudragalih94@gmail.com>
2023-07-04 11:46:07 +0700

    feat(Composition Root): Move RemoteLoader composition to Composition Root

--- EssentialFeed/EssentialFeed/Image Comments API/ImageCommentsMapper.swift
+++ EssentialFeed/EssentialFeed/Image Comments API/ImageCommentsMapper.swift
@@ -28,12 +28,16 @@ public final class ImageCommentsMapper {
         }
     }
     
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+    
     public static func map(_ data: Data, from response: HTTPURLResponse) throws -> [ImageComment] {
         let decoder = JSONDecoder()
         decoder.dateDecodingStrategy = .iso8601
         guard let root = try? decoder.decode(Root.self, from: data),
               isOK(response) else {
-            throw RemoteImageCommentsLoader.Error.invalidData
+            throw Error.invalidData
         }
         
         return root.comments
--- EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -45,7 +45,7 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
     private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> FeedLoader.Result? {
         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral)) // set configuration ephemeral so it doesn't store cache on url session
         // default folder cache is file:///Users/your-mac-username-here/Library/Caches/com.apple.dt.xctest.tool
-        let sut = RemoteFeedLoader(url: feedTestServerURL, client: client)
+        let sut = RemoteLoader(url: feedTestServerURL, client: client, mapper: FeedItemsMapper.map)
         trackForMemoryLeaks(client, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         var receivedResult: FeedLoader.Result?
