9aa5257a281cc861d63231e112db615ea7526ffd
galih <samudragalih94@gmail.com>
2023-07-19 15:49:35 +0700

    feat(FeedEndpoint): add `FeedEndpoint` with `after_id` param

--- EssentialFeed/EssentialFeed/Feed API/FeedEndpoint.swift
+++ EssentialFeed/EssentialFeed/Feed API/FeedEndpoint.swift
@@ -6,18 +6,19 @@
 //
 
 public enum FeedEndpoint {
-    case get
+    case get(after: FeedImage? = nil)
     
     public func url(baseURL: URL) -> URL {
         switch self {
-        case .get:
+        case let .get(feedImage):
             var components = URLComponents()
             components.scheme = baseURL.scheme
             components.host = baseURL.host()
             components.path = baseURL.path() + "/v1/feed"
             components.queryItems = [
-                URLQueryItem(name: "limit", value: "10")
-            ]
+                URLQueryItem(name: "limit", value: "10"),
+                feedImage.map{ URLQueryItem(name: "after_id", value: $0.id.uuidString) }
+            ].compactMap{$0}
             return components.url!
         }
     }
--- EssentialFeed/EssentialFeedTests/Feed API/FeedEndpointTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/FeedEndpointTests.swift
@@ -12,12 +12,25 @@ final class FeedEndpointTests: XCTestCase {
     func test_feed_endpointURL() {
         let baseURL = URL(string: "http://base-url.com")!
         
-        let receive: URL = FeedEndpoint.get.url(baseURL: baseURL)
+        let receive: URL = FeedEndpoint.get().url(baseURL: baseURL)
         
         XCTAssertEqual(receive.scheme, "http", "scheme")
         XCTAssertEqual(receive.host(), "base-url.com", "host")
         XCTAssertEqual(receive.path(), "/v1/feed", "path")
         XCTAssertEqual(receive.query(), "limit=10", "query")
         
+    }
+    
+    func test_feed_endpointURLAfterGivenImage() {
+        let baseURL = URL(string: "http://base-url.com")!
+        let feedImage = uniqueImage()
+        
+        let receive: URL = FeedEndpoint.get(after: feedImage).url(baseURL: baseURL)
+        
+        XCTAssertEqual(receive.scheme, "http", "scheme")
+        XCTAssertEqual(receive.host(), "base-url.com", "host")
+        XCTAssertEqual(receive.path(), "/v1/feed", "path")
+        XCTAssertEqual(receive.query()?.contains("limit=10"), true, "limit query param")
+        XCTAssertEqual(receive.query()?.contains("after_id=\(feedImage.id.uuidString)"), true, "after_id query param")
     }
 }
