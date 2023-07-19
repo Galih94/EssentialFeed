eef1f758f672b7ca8aff153429c9765f1da72fcc
galih <samudragalih94@gmail.com>
2023-07-19 10:54:51 +0700

    feat(Paginated): Does not load more after loading last page

--- EssentialApp/EssentialApp/CombineHelpers.swift
+++ EssentialApp/EssentialApp/CombineHelpers.swift
@@ -8,9 +8,24 @@
 import Combine
 import EssentialFeed
 
-extension Paginated {
-    var loadMorePublisher: ( () -> AnyPublisher<Self, Error> )? {
+public extension Paginated {
+    init(items: [Item], loadMorePublisher: (() -> AnyPublisher<Self, Error>)?) {
+        self.init(items: items, loadMore: loadMorePublisher.map { publisher in
+            return { completion in
+                publisher().subscribe(Subscribers.Sink(receiveCompletion: { result in
+                    if case let .failure(error) = result {
+                        completion(.failure(error))
+                    }
+                }, receiveValue: { result in
+                    completion(.success(result))
+                }))
+            }
+        })
+    }
+    
+    var loadMorePublisher: (() -> AnyPublisher<Self, Error>)? {
         guard let loadMore = loadMore else { return nil }
+        
         return {
             Deferred {
                 Future(loadMore)
